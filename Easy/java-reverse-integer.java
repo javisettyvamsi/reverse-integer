bbd9ccd92506e6d6270acc4a5651c769c0f8cd0b
Javisetty Vamsi <javisettyvamsi143@gmail.com>
2026-02-19 14:31:34 +0530

    Add solution for Reverse an Integer

---
+++ Easy/java-reverse-integer.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static int reverse(int n) {
+        // Your code here
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(reverse(sc.nextInt()));
+    }
+}
